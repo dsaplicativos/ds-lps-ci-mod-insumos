18c0a100c327e4aebf859bb50e0f8013162e00b2
Unknown <stobenetti@gmail.com>
2017-12-18 15:39:31 -0200

    Correção em inclusões de arquivos

--- controllers/Insumos.php
+++ controllers/Insumos.php
@@ -5,14 +5,14 @@ class Insumos extends MY_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('InsumoModel', 'model');
+        $this->load->model('insumos/InsumoModel', 'model');
     }
 
     function index() {
         $v = $this->model->getInsertPageData();
         $v['res'] = $this->model->newServiceInput();
         $data = $this->model->getIndexPageData();
-        $data['collapse'] = $this->load->view('service_inputs/newServiceInput', $v, true);
+        $data['collapse'] = $this->load->view('insumos/newServiceInput', $v, true);
         $this->show($this->load->view('tableView', $data, true));
     }
 
