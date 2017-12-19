224048ee207c2ac6417734a67cffdb6c15f502a6
Unknown <stobenetti@gmail.com>
2017-12-19 12:50:30 -0200

    Criação de páginas: inserir, editar, deletar

--- controllers/Insumos.php
+++ controllers/Insumos.php
@@ -7,13 +7,26 @@ class Insumos extends MY_Controller {
         parent::__construct();
         $this->load->model('insumos/InsumoModel', 'model');
     }
-
+    
     function index() {
         $v = $this->model->getInsertPageData();
         $v['res'] = $this->model->newServiceInput();
         $data = $this->model->getIndexPageData();
         $data['collapse'] = $this->load->view('insumos/newServiceInput', $v, true);
         $this->show($this->load->view('tableView', $data, true));
+    }
+
+    function edit($id) {
+        $v = $this->model->getEditPageData($id);
+        $v['res'] = $this->model->editServiceInput($id);
+        $data = $this->model->getIndexPageData(true);
+        $data['collapse'] = $this->load->view('insumos/newServiceInput', $v, true);
+        $this->show($this->load->view('tableView', $data, true));
+    }
+
+    function delete($id) {
+        $this->model->deleteServiceInput($id);
+        redirect('/insumos');
     }
 
 }
